b32abe3d82fffa4dcbc176c12a615c30ffc2fc86
AndreyDodonov <nordhummer@gmail.com>
2022-09-28 10:10:17 +0300

    feat: made routing with mux

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,8 +1,11 @@
 package apiserver
 
 import (
-	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
 
 type APIServer struct {
@@ -19,17 +22,20 @@ func New(config *Config) *APIServer {
 	}
 }
 
+// старт сервера
 func (s *APIServer) Start() error {
 	if err := s.configureLogger(); err != nil {
 		return err
 	}
 
+	s.configureRouter()
+
 	s.logger.Info("start API server")
 
-	return nil
+	return http.ListenAndServe(s.config.BindAddress, s.router)
 }
 
-func (s *APIServer) configureLogger() error  {
+func (s *APIServer) configureLogger() error {
 	level, err := logrus.ParseLevel(s.config.LogLevel)
 	if err != nil {
 		return err
@@ -39,6 +45,25 @@ func (s *APIServer) configureLogger() error  {
 	return nil
 }
 
-func (s *APIServer) configureRouter()  {
-	
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+	s.router.HandleFunc("/", s.handleMain())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	type request struct {
+		name string
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	}
+}
+
+func (s *APIServer) handleMain() http.HandlerFunc {
+	type request struct {
+		name string
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Main router")
+	}
 }
